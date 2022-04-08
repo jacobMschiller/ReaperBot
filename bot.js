2d5704019ef433cb26aaa33b311708a2fe202f21
RedFlame2112 <sagnik3@illinois.edu>
2022-04-08 11:42:45 -0500

    Added free reap functionality and fixed minor things

--- bot.js
+++ bot.js
@@ -20,4 +20,26 @@ function reapWhen(numSeconds) { // This function clicks the 'reap' button when t
 }
 
 const desiredTime = 500 // Measured in seconds.
-reapWhen(desiredTime)
+reapWhen(desiredTime)function getTime() { // This function gets the time that is currently on the timer.
+ 21   const bf = document.querySelector('#last-reap').textContent.split(' ');
+ 20   if (bf.length === 6) {
+ 19   ¦ return parseInt(bf[0], 10) * 3600 + parseInt(bf[2], 10) * 60 + parseInt(bf[4], 10);
+ 18   } else if (bf.length === 4) {
+ 17   ¦ return parseInt(bf[0], 10) * 60 + parseInt(bf[2], 10);
+ 16   }
+ 15   return parseInt(bf[0], 10);
+ 14 }
+ 13
+ 12 function reapWhen(numSeconds) { // This function clicks the 'reap' button when the timer reaches numSeconds.
+ 11   setInterval(()  => {
+ 10   ¦ const time = getTime();
+  9   ¦ console.log('Time available: ' + time);
+  8   ¦ if (time >= numSeconds && document.querySelector('#reap-button-container').style.display !== 'none') {
+  7   ¦ ¦ document.querySelector('#reap-button').click();
+  6   ¦ ¦ console.log('Click-Reaped!');
+  5   ¦ }
+  4   }, 5000);
+  3 }
+  2
+  1 const desiredTime = 500 // Measured in seconds.
+23  reapWhen(desiredTime)
